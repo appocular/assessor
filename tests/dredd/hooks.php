9b47c81cc4010f1508a49ac30d742d6430611f97
Thomas Fini Hansen <xen@xen.dk>
2018-11-10 23:56:14 +0100

    Add dredd documentation testing

---
+++ tests/dredd/hooks.php
@@ -0,0 +1,33 @@
+<?php
+
+use Dredd\Hooks;
+
+$stash = [];
+
+Hooks::beforeAll(function (&$transaction) {
+    putenv('DB_CONNECTION=sqlite');
+    putenv('DB_DATABASE=/tmp/assessor-dredd.sqlite');
+    exec(__DIR__ . '/../../artisan migrate:fresh');
+});
+
+Hooks::beforeEach(function (&$transaction) use (&$stash) {
+    // Replace the batch ID in requests with the last one we created.
+    if (isset($stash['batch_id'])) {
+        $replacements = array('58444f87-0525-429d-ba3c-d7b06cab748a' => $stash['batch_id']);
+        $transaction->request->uri = strtr($transaction->request->uri, $replacements);
+        $transaction->fullPath = strtr($transaction->fullPath, $replacements);
+    }
+
+    // Replace PNG data placeholder with real base64 encoded PNG data.
+    if (strpos($transaction->request->body, '<base64 PNG data>') !== false) {
+        $pngData = file_get_contents(__DIR__ . '/../../fixtures/images/basn6a16.png');
+        $replacements = array('<base64 PNG data>' => base64_encode($pngData));
+        $transaction->request->body = strtr($transaction->request->body, $replacements);
+    }
+});
+
+Hooks::after('Batch resource > Create batch', function (&$transaction) use (&$stash) {
+    // Save created batch ID for use in subsequent tests.
+    $json_data = json_decode($transaction->real->body);
+    $stash['batch_id'] = $json_data->id;
+});
