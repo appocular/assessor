683ef5372015d2bc2a834c3b97cae272d9e420ef
Thomas Fini Hansen <xen@xen.dk>
2018-12-06 14:40:34 +0100

    Test API body documentation

--- tests/dredd/hooks.php
+++ tests/dredd/hooks.php
@@ -26,6 +26,31 @@ Hooks::beforeEach(function (&$transaction) use (&$stash) {
     }
 });
 
+Hooks::afterEach(function (&$transaction) use (&$stash) {
+    // Check that the JSON payload matches the documentation.
+    if (isset($transaction->real->body) || isset($transaction->expected->body)) {
+        $actual = json_encode(array_sort_recursive(
+            json_decode($transaction->real->body, true)
+        ));
+
+        $expected = array_sort_recursive(
+            json_decode($transaction->expected->body, true)
+        );
+
+        // For the batch creation call, expect the ID that was just returned.
+        if ($transaction->fullPath == '/batch' &&
+            isset($expected['id']) &&
+            $expected['id'] == '58444f87-0525-429d-ba3c-d7b06cab748a') {
+            $expected['id'] = $stash['batch_id'];
+        }
+        $expected = json_encode($expected);
+
+        if ($actual != $expected) {
+            $transaction->fail = "Difference in JSON payload.";
+        }
+    }
+});
+
 Hooks::after('Batch resource > Create batch', function (&$transaction) use (&$stash) {
     // Save created batch ID for use in subsequent tests.
     $json_data = json_decode($transaction->real->body);
