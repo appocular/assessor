5600010444c5da3050a0fa4cd77ee1fd38c30815
Thomas Fini Hansen <xen@xen.dk>
2018-12-23 00:48:33 +0100

    Add proper setup in dredd

--- scripts/dredd-server.sh
+++ scripts/dredd-server.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+export APP_ENV=local
 export DB_CONNECTION=sqlite
 export DB_DATABASE=/tmp/assessor-dredd.sqlite
+export CACHE_DRIVER=file
 export KEEPER_BASE_URI=http://localhost:8081/
 
 if [[ ! -d keeper ]]; then
