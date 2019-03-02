ed2e60995b2e177ad1cc6d3030978b712b8d8677
Thomas Fini Hansen <xen@xen.dk>
2019-03-02 22:28:37 +0100

    Don't queue checkpoint baseline reset

--- app/Listeners/ResetCheckpointBaselines.php
+++ app/Listeners/ResetCheckpointBaselines.php
@@ -5,11 +5,9 @@ namespace Appocular\Assessor\Listeners;
 use Appocular\Assessor\Events\SnapshotUpdated;
 use Appocular\Assessor\Checkpoint;
 use Appocular\Assessor\Snapshot;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
 
-class ResetCheckpointBaselines implements ShouldQueue
+class ResetCheckpointBaselines
 {
     /**
      * Handle the event.
