02c853cb2af35b4c62dfb993cd5256377e54cd70
Thomas Fini Hansen <xen@xen.dk>
2019-06-01 21:31:39 +0200

    Add some error handling around diff submission

--- app/Observers/CheckpointObserver.php
+++ app/Observers/CheckpointObserver.php
@@ -5,6 +5,7 @@ namespace Appocular\Assessor\Observers;
 use Appocular\Assessor\Checkpoint;
 use Appocular\Clients\Contracts\Differ;
 use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class CheckpointObserver
 {
@@ -37,8 +38,22 @@ class CheckpointObserver
             !empty($checkpoint->image_sha) &&
             !empty($checkpoint->baseline_sha) &&
             $checkpoint->isDirty('image_sha', 'baseline_sha')) {
-            Log::info(sprintf('Submitting diff for image %s, baseline %s', $checkpoint->image_sha, $checkpoint->baseline_sha));
-            $this->differ->submit($checkpoint->image_sha, $checkpoint->baseline_sha);
+            Log::info(sprintf(
+                'Submitting diff for image %s, baseline %s',
+                $checkpoint->image_sha,
+                $checkpoint->baseline_sha
+            ));
+            try {
+                $this->differ->submit($checkpoint->image_sha, $checkpoint->baseline_sha);
+            } catch (Throwable $e) {
+                Log::error(sprintf(
+                    'Error submitting diff image %s, baseline %s: %s',
+                    $checkpoint->image_sha,
+                    $checkpoint->baseline_sha,
+                    $e->getMessage()
+                ));
+            }
+            Log::debug('Submitted');
         }
 
         // Update snapshot status when checkpoint status changes.
