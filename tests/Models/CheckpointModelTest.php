accdcdf9ff8cdac0dea8802351d72914318354c9
Thomas Fini Hansen <xen@xen.dk>
2019-06-16 20:20:13 +0200

    Add Checkpoint model test

---
+++ tests/Models/CheckpointModelTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Models;
+
+use Appocular\Assessor\Checkpoint;
+use Appocular\Assessor\Snapshot;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+
+class CheckpointModelTest extends \TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @dataProvider statusProvider
+     */
+    public function testBulkDiffUpdatesSetsStatusesCorrectly(
+        $name,
+        $existingStatus,
+        $existingDiffStatus,
+        $existingDiffSha,
+        $difference,
+        $expectedStatus,
+        $expectedDiffStatus,
+        $expectedDiffSha
+    ) {
+        $snapshot = factory(Snapshot::class)->create();
+        factory(Checkpoint::class)->create([
+            'snapshot_id' => $snapshot->id,
+            'name' => $name,
+            'image_sha' => 'image',
+            'baseline_sha' => 'baseline',
+            'diff_sha' => $existingDiffSha,
+            'status' => $existingStatus,
+            'diff_status' => $existingDiffStatus,
+        ]);
+
+        Checkpoint::updateDiffs('image', 'baseline', 'new_diff', $difference);
+
+        $this->seeInDatabase('checkpoints', [
+            'name' => $name,
+            'status' => $expectedStatus,
+            'diff_status' => $expectedDiffStatus,
+            'diff_sha' => $expectedDiffSha,
+        ]);
+    }
+
+    public function statusProvider()
+    {
+        return [
+            // First the happy path, updating checkpoints without a diff.
+            [
+                'unknown',
+                Checkpoint::STATUS_UNKNOWN,
+                Checkpoint::DIFF_STATUS_UNKNOWN,
+                null,
+                true,
+                Checkpoint::STATUS_REJECTED,
+                Checkpoint::DIFF_STATUS_DIFFERENT,
+                'new_diff',
+            ],
+            [
+                'unknown',
+                Checkpoint::STATUS_UNKNOWN,
+                Checkpoint::DIFF_STATUS_UNKNOWN,
+                null,
+                false,
+                Checkpoint::STATUS_APPROVED,
+                Checkpoint::DIFF_STATUS_IDENTICAL,
+                'new_diff',
+            ],
+            // Check that existing diffs doesn't get overwritten.
+            [
+                'unknown',
+                Checkpoint::STATUS_APPROVED,
+                Checkpoint::DIFF_STATUS_DIFFERENT,
+                'diff',
+                true,
+                Checkpoint::STATUS_APPROVED,
+                Checkpoint::DIFF_STATUS_DIFFERENT,
+                'diff',
+            ],
+        ];
+    }
+}
