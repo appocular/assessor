e02c5295b986c908a340cd2b24d3ad9fb9989230
Thomas Fini Hansen <xen@xen.dk>
2019-09-04 22:17:02 +0200

    Don't check response payload in API tests
    Now that we're properly defining datatypes, it'll check for the right
    structure. That the calls return the proper data is another tests
    responsibility.

--- tests/dredd/hooks.php
+++ tests/dredd/hooks.php
@@ -4,13 +4,13 @@ use Dredd\Hooks;
 
 $stash = [];
 
-Hooks::beforeAll(function (&$transaction) {
+Hooks::beforeAll(function ($transaction) {
     putenv('DB_CONNECTION=sqlite');
     putenv('DB_DATABASE=/tmp/assessor-dredd.sqlite');
     exec(__DIR__ . '/../../artisan migrate:fresh');
 });
 
-Hooks::beforeEach(function (&$transaction) use (&$stash) {
+Hooks::beforeEach(function ($transaction) use (&$stash) {
     // Replace the batch ID in requests with the last one we created.
     if (isset($stash['batch_id'])) {
         $replacements = array('58444f87-0525-429d-ba3c-d7b06cab748a' => $stash['batch_id']);
@@ -26,55 +26,7 @@ Hooks::beforeEach(function (&$transaction) use (&$stash) {
     }
 });
 
-Hooks::afterEach(function (&$transaction) use (&$stash) {
-    // Dry run support.
-    if (!isset($transaction->real)) {
-        return;
-    }
-    $replacements = array('58444f87-0525-429d-ba3c-d7b06cab748a' => $stash['batch_id']);
-
-    // Check that the headers matches the documentation.
-    if (!empty($transaction->expected->headers)) {
-        foreach ($transaction->expected->headers as $name => $content) {
-            // Replace documentation batch id with the current one.
-            $content = strtr($content, $replacements);
-            if (!isset($transaction->real->headers->{strtolower($name)})) {
-                $transaction->fail = "No $name header in reply.";
-            }
-            elseif ($transaction->real->headers->{strtolower($name)} != $content) {
-                $transaction->fail = "Difference in $name header payload.";
-            }
-        }
-    }
-
-    // Check that the payload matches the documentation.
-    if (!empty($transaction->expected->body)) {
-        $contentType = isset($transaction->expected->headers->{"Content-Type"}) ?
-            $transaction->expected->headers->{"Content-Type"} : "";
-        switch ($contentType) {
-            case 'application/json':
-                $actual = normalize_json($transaction->real->body);
-                $expected = normalize_json($transaction->expected->body);
-                break;
-
-            default:
-                // Simple comparison for everything else. This includes
-                // text/plain which dredd apparently checks itself, but what
-                // the hell, we'll check it too.
-                $actual = $transaction->real->body;
-                $expected = $transaction->expected->body;
-        }
-
-        // Replace documentation batch id with the current one.
-        $expected = strtr($expected, $replacements);
-
-        if ($actual != $expected) {
-            $transaction->fail = "Difference in payload.";
-        }
-    }
-});
-
-Hooks::after('Batch resource > Create batch > Example 1', function (&$transaction) use (&$stash) {
+Hooks::after('Batch resource > Create batch > Example 1', function ($transaction) use (&$stash) {
     // Dry run support.
     if (!isset($transaction->real)) {
         return;
@@ -83,19 +35,6 @@ Hooks::after('Batch resource > Create batch > Example 1', function (&$transactio
     if (isset($transaction->real->headers->location)) {
         $parts = explode('/', $transaction->real->headers->location);
         $id = array_pop($parts);
-        print_r($id);
         $stash['batch_id'] = $id;
     }
-    // $json_data = json_decode($transaction->real->body);
 });
-
-function normalize_json($json)
-{
-    if (!empty($json)) {
-        $data = json_decode($json, true);
-        if (is_array($data)) {
-            return json_encode(array_sort_recursive($data));
-        }
-    }
-    return "";
-}
